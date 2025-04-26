a20efe4cc426e9e4ec73a15ff2f539aab2765ea7
Андрей <adlerSt@yandex.ru>
2025-04-26 12:40:29 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.HashSet;
 public class Main {
     public static void main(String[] args) {
         int[] nums = {1, 5, 8, 34, 143, 56, 98, 14, 26, 11, 33, 95, 3, 41, 54, 90};
-        int targetSum = 44;
+        int targetSum = 13;
         HashSet<Integer> numbers = new HashSet<>();
 
         for (int i = 0; i < nums.length; i++) {
@@ -11,6 +11,7 @@ public class Main {
                 for (int j = 0; j < i; j++) {
                     if (nums[j] == targetSum - nums[i]) {
                         System.out.println("Индексы: " + j + ", " + i);
+                        System.out.println("Числа " + nums[i] + " " + nums[j]);
                         return;
                     }
                 }
